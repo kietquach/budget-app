c39e4c00d672ba5a22721cf1af36d2b9deb6b2c1
kietquach <kvquach@ucdavis.edu>
2018-12-24 08:43:24 -0800

    added react-redux with example connection in ExpenseList

--- src/app.js
+++ src/app.js
@@ -1,7 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
+import { Provider } from "react-redux";
+import { addExpense } from "./actions/expenses";
+import { setTextFilter } from "./actions/filters";
+import getVisibleState from "./selectors/expenses";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-ReactDOM.render(<AppRouter />, document.getElementById("app"));
+const store = configureStore();
+
+store.subscribe(() => {
+    const state = store.getState()
+    const visibleState = getVisibleState(state.expenses, state.filters);
+    console.log(visibleState);
+});
+
+store.dispatch(addExpense({ description: "Water bill", amount: 10000, createdAt: 20000 }));
+store.dispatch(addExpense({ description: "Gas bill", amount: 5000, createdAt: 25000 }));
+store.dispatch(setTextFilter("gas"));
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById("app"));
